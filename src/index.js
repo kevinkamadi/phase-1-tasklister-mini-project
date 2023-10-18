06768dd963140454ab805eded1f600f3da4ce00b
Your Name <kkamadi567@gmail.com>
2023-10-18 14:46:30 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+ form.addEventListener("submit", function(event){
+    event.preventDefault()
+    buildTodo(form["new-task-description"].value)
+    form.reset();
+  })
+
+  function buildTodo(todo){
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", handleRemove)
+    btn.textContent = "x"
+    p.textContent= `${todo} `
+    p.appendChild(btn)
+    document.querySelector("#tasks").appendChild(p)
+  }
+  function handleRemove(e){
+    e.target.parentNode.remove()
+  }
 });
